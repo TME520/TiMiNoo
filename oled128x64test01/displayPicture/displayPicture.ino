6a188ce37212de6933c030cf22189cab8c5b0186
TME520 HP14 <tme520@gmail.com>
2023-05-20 19:56:14 +1000

    2 frames cat anim + button is active

--- oled128x64test01/displayPicture/displayPicture.ino
+++ oled128x64test01/displayPicture/displayPicture.ino
@@ -19,16 +19,12 @@ VCC = 3V
 SIG = 2
 */
 
-const uint8_t rook_bitmap[] U8G_PROGMEM = {
-  0x00,         // 00000000 
-  0x55,         // 01010101
-  0x7f,          // 01111111
-  0x3e,         // 00111110
-  0x3e,         // 00111110 
-  0x3e,         // 00111110
-  0x3e,         // 00111110 
-  0x7f           // 01111111
-};
+int currentFrame = 0;
+const char* textToDisplay = "";
+const int buttonPin = 2;
+int buttonState = 0;
+int counter = 0;
+char counterText[4];
 
 #define cat_sitting_001_width 12
 #define cat_sitting_001_height 12
@@ -36,23 +32,48 @@ static unsigned char cat_sitting_001_bits[] U8G_PROGMEM = {
    0x60, 0x0c, 0xa0, 0x0b, 0x20, 0x08, 0xa0, 0x0c, 0x20, 0x08, 0x20, 0x08,
    0x10, 0x04, 0x10, 0x04, 0x10, 0x04, 0x8e, 0x0a, 0x89, 0x0a, 0xff, 0x0f };
 
-void draw(void) {
-  // graphic commands to redraw the complete screen should be placed here  
-  // u8g.drawBitmapP( 0, 0, 1, 8, rook_bitmap);
-  // u8g.drawBitmapP( 0, 0, 12, 12, catsitting001);
-  u8g.drawXBMP(0, 0, cat_sitting_001_width, cat_sitting_001_height, cat_sitting_001_bits);
+#define cat_sitting_002_width 12
+#define cat_sitting_002_height 12
+static unsigned char cat_sitting_002_bits[] U8G_PROGMEM = {
+   0x60, 0x0c, 0xa0, 0x0b, 0x20, 0x08, 0xa0, 0x0c, 0x20, 0x08, 0x20, 0x08,
+   0x10, 0x04, 0x10, 0x04, 0x1c, 0x04, 0x8a, 0x0a, 0x8d, 0x0a, 0xfb, 0x0f };
+
+void draw(int frameId, const char* text, int posx, int posy) {
+  // graphic commands to redraw the complete screen should be placed here
+  u8g.setFont(u8g_font_unifont);
+  u8g.drawStr(posx, posy, text);
+  if ( frameId == 0 ) {
+    u8g.drawXBMP(0, 0, cat_sitting_001_width, cat_sitting_001_height, cat_sitting_001_bits);
+  } else if ( frameId == 1 ) {
+    u8g.drawXBMP(0, 0, cat_sitting_002_width, cat_sitting_002_height, cat_sitting_002_bits);
+  }
 }
 
 void setup(void) {
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
   // flip screen, if required
   u8g.setRot180();
 }
 
 void loop(void) {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    // Button pressed
+    currentFrame = 0;
+    counter = 0;
+  }
   // picture loop
+  counter += 1;
+  itoa(counter, counterText, 10);
+  currentFrame += 1;
+  if ( currentFrame > 1 ) {
+    currentFrame = 0;
+  }
   u8g.firstPage();  
   do {
-    draw();
+    draw(currentFrame, counterText, 0, 50);
   } while( u8g.nextPage() );
   
   // rebuild the picture after some delay
